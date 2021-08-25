46d29dfdf73b5c770541535dc38537d68fc49b4a
verbocamacho <verbocamacho@gmail.com>
2021-08-25 14:48:00 -0500

    Files Generated 1 and 2

---
+++ PrimerP/src/main/java/edu/eci/escuelaing/ieti/PrimerP/PrimerPApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.escuelaing.ieti.PrimerP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerPApplication.class, args);
+	}
+
+}
---
+++ PrimerP/src/test/java/edu/eci/escuelaing/ieti/PrimerP/PrimerPApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.escuelaing.ieti.PrimerP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
